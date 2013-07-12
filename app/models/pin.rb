19c39df8dbd5173795d15ba515e27c83d795d2cd
Tom Kim <tom.y.kim@gmail.com>
2013-07-12 13:13:23 -0700

    Fix Amazon S3

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,4 +1,7 @@
+#require 'open-uri'
+
 class Pin < ActiveRecord::Base
+
   attr_accessible :description, :image, :image_remote_url
 
   validates :description, presence: true
@@ -11,7 +14,7 @@ class Pin < ActiveRecord::Base
   has_attached_file :image, styles: { medium: "320x240>", thumb: "100x100>" }
 
   def image_remote_url=(url_value)
-  	self.image = URI.parse(url_value) unless (url_value.blank?)
+  	self.image = URI.parse(url_value) unless url_value.blank?
   	super
   end
 end	
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -39,13 +39,13 @@ Omrails::Application.configure do
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 
   # Allows Amazon S3 connection for Paperclip file upload
-  config.paperclip_defaults = {
-    :storage => :s3,
-    :s3_credentials => {
-      :bucket => ENV['footprint-omrails'],
-      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
-    }
+  #config.paperclip_defaults = {
+  # :storage => :s3,
+  # :s3_credentials => {
+  #   :bucket => ENV['footprint-omrails'],
+  #   :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+  #   :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+  # }
   }
 
 end
